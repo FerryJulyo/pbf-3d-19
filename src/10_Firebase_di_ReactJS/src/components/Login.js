56b3e533c247ec9324795a54943e00bede1b8af4
FerryJulyo <ferryjulyo86@gmail.com>
2021-05-04 10:33:22 +0700

    update 10

--- src/10_Firebase_di_ReactJS/src/components/Login.js
+++ src/10_Firebase_di_ReactJS/src/components/Login.js
@@ -1,62 +1,104 @@
-import { useContext, useState } from "react";
-import {AuthContext} from "../App";
-
+import { useContext, useState } from 'react';
+import { AuthContext } from '../App';
 import firebase from 'firebase/app';
 import "firebase/analytics";
 import "firebase/auth";
+import { Spinner } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+var provider = new firebase.auth.GoogleAuthProvider();
 
 function Login() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setErrors] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [Loading2, setLoading2] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setErrors] = useState("");
 
-    const Auth = useContext(AuthContext);
+  const Auth = useContext(AuthContext);
 
-    const handleForm = e => {
-        e.preventDefault();
+  const handleOnClick = (provider) => {
+    setLoading2(true);
+    firebase.auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        if (result.user) Auth.setLoggedIn(true);
 
-        firebase
-            .auth()
-            .signInWithEmailAndPassword(email, password)
-            .then(res => {
-                if (res.user) Auth.setLoggedIn(true)
-            })
-            .catch(e => {
-                setErrors(e.message);
-            })
-    }
+        /** @type {firebase.auth.OAuthCredential} */
+        var credential = result.credential;
 
-    return (
-        <div>
-          <h1>Login</h1>
-          <form onSubmit={e => handleForm(e)}>
-            <input
-              vAalue={email}
-              onChange={e => setEmail(e.target.value)}
-              name="email"
-              type="email"
-              placeholder="email"
-            />
-            <input
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-              name="password"
-              type="password"
-              placeholder="password"
-            />
-            <hr />
-            <button className="googleBtn" type="button">
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-                alt="logo"
-              />
-              Login With Google
-            </button>
-            <button type="submit">Login</button>
-            <span>{error}</span>
-          </form>
-        </div>
-      );
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        setLoading2(false);
+        // ...
+      }).catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+  }
+
+  const handleForm = e => {
+    setLoading(true);
+    e.preventDefault();
+    // console.log(Auth);
+    // Auth.setLoggedIn(true);
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(res => {
+        if (res.user) Auth.setLoggedIn(true);
+        setLoading(false);
+      })
+      .catch(e => {
+        setErrors(e.message);
+      })
+  };
+
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={e => handleForm(e)}>
+        <input
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          name="email"
+          type="email"
+          placeholder="email"
+        />
+        <input
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          name="password"
+          type="password"
+          placeholder="password"
+        />
+        <hr />
+        <button className="googleBtn" type="button" onClick={() => handleOnClick(provider)}>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+            alt="logo"
+          />
+          {Loading2 ? <Spinner role="status" animation="border" variant="primary" /> : "Login With Google"}
+
+        </button>
+        <button type="submit">
+          {loading ? <Spinner role="status" animation="border" variant="primary" /> : "Login"}
+        </button>
+        <span>{error}</span>
+      </form>
+    </div>
+  );
 }
+
 
 export default Login;
