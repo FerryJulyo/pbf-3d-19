72bd81812f08c7713ce38288e5dbddbc3cf3006f
FerryJulyo <ferryjulyo86@gmail.com>
2021-05-24 12:00:17 +0700

    update

---
+++ src/11_Redux_Thunk/src/firebase.config.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBauWx4LYTuyQHsLdelB9wYpHnnjq2vJOU",
+  authDomain: "belajar-react-8c315.firebaseapp.com",
+  projectId: "belajar-react-8c315",
+  storageBucket: "belajar-react-8c315.appspot.com",
+  messagingSenderId: "852588152528",
+  appId: "1:852588152528:web:ad9031114ed8765fdc00d5",
+  measurementId: "G-52X7YV91NY"
+};
+
+export const myFirebase = firebase.initializeApp(firebaseConfig);
+
